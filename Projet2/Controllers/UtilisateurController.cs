7e7f198adff3429ce5bd0117d73666510563f9f3
Ibtissem <eqlrepository@gmail.com>
2021-01-22 10:11:55 +0100

    push bug ibtissem
    PushBug
    
    Bug ViewBag
    
    Ajoutez des fichiers projet.
    
    Ajoutez les fichiers .gitignore et .gitattributes.

---
+++ Projet2/Controllers/UtilisateurController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using Projet2.Models;
+
+
+namespace Projet2.Controllers
+{
+    public class UtilisateurController : Controller
+    {
+        pandamidbEntities db = new pandamidbEntities();
+    
+       
+        public ActionResult Index()
+        {
+         return View();
+        }
+        public ActionResult Utilisateur()
+        {
+           
+               
+
+
+            
+            try
+            {
+                ViewBag.Title = "Utilisateur";
+                ViewBag.listeUtilisateur = db.utilisateur.ToList();
+                return View();
+            }
+            catch(Exception e)
+            {
+                return HttpNotFound();
+            }
+           
+        }
+        [HttpPost]
+        public ActionResult AjoutUtilisateur(utilisateur utilisateur)
+        {
+            try
+            {
+              if(ModelState.IsValid)
+                {
+                    ViewBag.listeUtilisateur = db.utilisateur.ToList();
+                    utilisateur.date_inscription = DateTime.Now;
+                    db.utilisateur.Add(utilisateur);
+                    db.SaveChanges();
+                }
+                
+                return Utilisateur();
+            }
+            catch(Exception e)
+            {
+                return HttpNotFound();
+            }
+         
+            
+        }
+           
+
+    }
+}
